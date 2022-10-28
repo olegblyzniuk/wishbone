b897f69037787d014384b26c6a3b5f63c9b504d0
olegblyzniuk <mettal062@gmail.com>
2022-10-28 23:17:15 +0300

    media 834px

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,16 @@ $('.owl-carousel').owlCarousel({
     loop:true,
     margin:0,
     nav:true,
-    navText: [ '', ' ' ],
+    navText: [ '', '' ],
     autoplay: true,
     autoplayTimeout:2000,
     responsive:{
         0:{
-            items:2
+            items:1
         },
 
         1000:{
-            items:5
+            items:3
         }
     }
 });
